4a10cb1576709404bc4f0ca13469f61dc1450944
Roman Starkov <sourcecode@roman.st>
2021-05-17 15:36:48 +0100

    Weird speed change

--- Program.cs
+++ Program.cs
@@ -1,32 +1,21 @@
 using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using RT.Util;
 
 namespace SieveEratosthenes
 {
-    unsafe class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            double minTime = double.MaxValue;
-            long minCycles = long.MaxValue;
-            while (true)
-            {
-                var c = new TicTocCycles();
-                Ut.Tic();
-                c.Tic();
-                var map = new PrimeMap();
-                minCycles = Math.Min(minCycles, c.Toc());
-                minTime = Math.Min(minTime, Ut.Toc());
-                Console.WriteLine($"{minTime * 1000:#,0} ms, {minCycles:#,0} cycles");
+            var start = DateTime.UtcNow;
+            var map = new PrimeMap();
+            Console.WriteLine($"{(DateTime.UtcNow - start).TotalMilliseconds:#,0} ms");
 
-                //for (int i = 0; i < 20000; i++)
-                //    if (map.IsPrime(i))
-                //        Console.WriteLine(i);
+            map.Dispose(); // comment this out and watch the speed change
 
-                map.Dispose();
-            }
+            start = DateTime.UtcNow;
+            map = new PrimeMap();
+            Console.WriteLine($"{(DateTime.UtcNow - start).TotalMilliseconds:#,0} ms");
         }
     }
 
@@ -52,17 +41,6 @@ namespace SieveEratosthenes
         public void Dispose()
         {
             Marshal.FreeHGlobal((IntPtr) _composite);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool IsPrime(int value)
-        {
-            if (value == 2)
-                return true;
-            if (value < 2 || (value & 1) == 0)
-                return false;
-            ulong c = _composite[value >> 7]; // 1 for even numbers + 6 for one of the 64 bits per ulong
-            return ((c >> ((value >> 1))) & 1) == 0;
         }
     }
 }
